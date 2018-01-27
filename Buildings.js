0c11eb51f277492a5a13ce991f91d665b806eb95
drb4637 <drb4637@gmail.com>
2018-01-27 12:41:04 -0500

    added a thing

--- Buildings.js
+++ Buildings.js
@@ -1,12 +1,26 @@
 
-function platform(sprite) 
+
+function TileRow(sprite, group, amount, X, Y) 
 {
-    platforms = game.add.group();
+	var platform = group.add.group();	
+	for(var i = 0 ; i< amount; i++) 
+	{
+		var p= 	platform.create(X, Y, sprite);	
+		p.body.immovable = true;
+		X = X + 25; 
+	}	
+}
+
+function MakePlatform(sprite, game) 
+{
+    var platforms = game.add.group();
     platforms.enableBody = true;
 
     const ground = platforms.create(0, game.world.height - 64, sprite);
     ground.scale.setTo(2, 2);
     ground.body.immovable = true;
+
+	TileRow
 
     const ledgeA = platforms.create(400, 400, sprite);
     ledgeA.body.immovable = true;
